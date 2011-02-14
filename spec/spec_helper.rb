d712b76750846399f8afacb0a23690259e5d88fa
Marco Scholl <develop@marco-scholl.de>
2011-02-14 19:51:57 +0100

    cleanup and some gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'simplecov'
+SimpleCov.start 'rails'
 require 'rspec'
 require 'rails_paginate'
 
