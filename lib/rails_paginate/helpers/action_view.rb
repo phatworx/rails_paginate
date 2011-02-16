2bdef683a266471944ceaf8a3bf33a5fbec9694d
Marco Scholl <develop@marco-scholl.de>
2011-02-16 21:43:07 +0100

    view spec

--- lib/rails_paginate/helpers/action_view.rb
+++ lib/rails_paginate/helpers/action_view.rb
@@ -2,6 +2,9 @@ module RailsPaginate::Helpers
   module ActionView
     def paginate(*args)
       options = args.extract_options!
+
+      raise ArgumentError, "first argument muss be a RailsPaginate::Collection" unless args.first.is_a? RailsPaginate::Collection
+
       "<b>html</b>".html_safe
     end
   end
--- spec/helpers/action_view_spec.rb
+++ spec/helpers/action_view_spec.rb
@@ -1,6 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe RailsPaginate::Helpers::ActionView do
-  subject { ActionView::Base.new }
-  specify { should respond_to :paginate }
+  context "ActionView::Base.new" do
+    let(:action_view) { ActionView::Base.new }
+    subject { action_view }
+    specify { should respond_to :paginate }
+
+    it "#paginate should raise an error when no collection given" do
+      lambda { subject.paginate }.should raise_error(ArgumentError)
+    end
+
+    [
+        {:items => 100, :page => 1, :per_page => 15},
+        {:items => 300, :page => 2, :per_page => 50},
+        {:items => 11, :page => 5, :per_page => 15},
+        {:items => 0, :page => 1, :per_page => 15},
+        {:items => 12, :page => 1, :per_page => 15}
+    ].each do |config|
+      context "#paginate an Array with #{config[:items]} items, page #{config[:page]}, per_page #{config[:per_page]}" do
+        before do
+          @collection = (1..config[:items]).to_a.paginate :page => config[:page], :per_page => config[:per_page]
+          @paginator = action_view.paginate @collection
+        end
+        subject { @paginator }
+        it("return should be a kind of String") { should be_a String }
+      end
+    end
+  end
 end
