609c7a49f547b19662abd1820b7efde2088235a5
Marco Scholl <develop@marco-scholl.de>
2011-02-17 16:58:29 +0100

    update helpers

--- lib/rails_paginate/renderers/base.rb
+++ lib/rails_paginate/renderers/base.rb
@@ -1,6 +1,11 @@
 module RailsPaginate::Renderers
   # base method
   class Base
+    cattr_accessor :inner_window, :outer_window
+
+    @@inner_window = 2
+    @@outer_window = 2
+
     attr_reader :view, :collection, :options
 
     def initialize(view, collection, options = {})
@@ -18,30 +23,89 @@ module RailsPaginate::Renderers
       rescue
         "?page=#{page}"
       end
-
     end
 
     def render
       raise StandardError, "render is not implemented"
     end
 
-    protected
-
-    def link_to_page(page, key, options = {})
-      css_class = "link #{options[:class]} #{page == collection.current_page ? 'current' : ''}"
-      if page.nil?
-        view.content_tag :span, :class => css_class do
-          view.content_tag :span, :class => "inline" do
-            view.t(key)
-          end
-        end
-      else
-        view.link_to url_for_page(page), :class => css_class, :alt => view.strip_tags(view.t(key, :page => page)) do
-          view.content_tag :span, :class => "inline" do
-            view.t(key, :page => page)
+    def visible_pages
+      visible = []
+      last_found = 0
+      split = false
+      (1..collection.pages).each do |p|
+        # insert
+        if page_visible? p
+          visible << p
+          last_found = p
+          split = false
+        else
+          # need splitter
+          if not split and outer_window > 0 and last_found < p
+            visible << nil
+            split = true
           end
         end
       end
+      visible
+    end
+
+    def inner_window_range
+      @inner_window_range ||= (current_page - inner_window)..(current_page + inner_window)
+    end
+
+    def page_visible?(p)
+      # outer
+      if outer_window > 0
+        return true if outer_window >= p
+        return true if (collection.pages - outer_window) < p
+      end
+
+      # current page
+      return true if current_page == p
+
+      # inner
+      return true if inner_window_range.include? p
+
+      false
+    end
+
+    protected
+
+    def link_to_page(page, key, options = {})
+      css_class = "#{options[:class]} #{page == current_page ? 'current' : ''}"
+      if key.nil?
+        content_tag :span, "..", :class => "spacer"
+      elsif page.nil?
+        content_tag :span, t(key), :class => "#{css_class} unavailable"
+      else
+        link_to t(key, :page => page), url_for_page(page), :class => css_class, :alt => view.strip_tags(t(key, :page => page))
+      end
+    end
+
+    # helper
+    def current_page
+      collection.current_page
+    end
+
+    def content_tag(*args, &block)
+      view.content_tag(*args, &block)
+    end
+
+    def link_to(*args, &block)
+      view.link_to(*args, &block)
+    end
+
+    def t(*args)
+      view.t(*args)
+    end
+
+    def inner_window
+      self.class.inner_window
+    end
+
+    def outer_window
+      self.class.outer_window
     end
 
   end
--- lib/rails_paginate/renderers/html_default.rb
+++ lib/rails_paginate/renderers/html_default.rb
@@ -1,35 +1,46 @@
 module RailsPaginate::Renderers
   # normale renderer
   class HtmlDefault < Base
+
+
     def render
       view.content_tag(:ul) do
         html = "\n"
-        html += view.content_tag(:li, :class => "first_page") do
+        html += content_tag(:li, :class => "first_page") do
           link_to_page collection.first_page, 'paginate.first_page_label'
         end
         html += "\n"
-        html += view.content_tag(:li, :class => "previous_page") do
+        html += content_tag(:li, :class => "previous_page") do
           link_to_page collection.previous_page, 'paginate.previous_page_label'
         end
 
-        (1..collection.pages).each do |page|
-          html += "\n"
-          html += view.content_tag(:li, :class => "pager") do
-            link_to_page page, 'paginate.pager'
-          end
-        end
+        html += render_pages
 
         html += "\n"
-        html += view.content_tag(:li, :class => "next_page") do
+        html += content_tag(:li, :class => "next_page") do
           link_to_page collection.next_page, 'paginate.next_page_label'
         end
         html += "\n"
-        html += view.content_tag(:li, :class => "last_page") do
+        html += content_tag(:li, :class => "last_page") do
           link_to_page collection.last_page, 'paginate.last_page_label'
         end
         html += "\n"
         html.html_safe
       end
     end
+
+    def render_pages
+      html = ""
+
+      visible_pages.to_a.each do |page|
+        html += "\n"
+        html += view.content_tag(:li, :class => "pager") do
+          link_to_page page, page.nil? ? nil : 'paginate.pager'
+        end
+      end
+      html += "\n"
+      html.html_safe
+    end
+
   end
 end
