8d409d356c25db62f568d972e8c1670d4ed23e5f
Marco Scholl <develop@marco-scholl.de>
2011-02-17 17:55:28 +0100

    work an randerer

--- lib/rails_paginate/renderers/html_default.rb
+++ lib/rails_paginate/renderers/html_default.rb
@@ -1,37 +1,50 @@
 module RailsPaginate::Renderers
   # normale renderer
   class HtmlDefault < Base
+    # show first page item
+    cattr_accessor :show_first_page
+    @@show_first_page = true
 
+    # show last page item
+    cattr_accessor :show_last_page
+    @@show_last_page = true
 
     def render
-      view.content_tag(:ul) do
+      content_tag(:ul) do
         html = "\n"
-        html += content_tag(:li, :class => "first_page") do
-          link_to_page collection.first_page, 'paginate.first_page_label'
+
+        if show_first_last_page?
+          html += content_tag(:li, :class => "first_page") do
+            link_to_page collection.first_page, 'paginate.first_page_label'
+          end
+          html += "\n"
         end
-        html += "\n"
+
         html += content_tag(:li, :class => "previous_page") do
           link_to_page collection.previous_page, 'paginate.previous_page_label'
         end
 
-        html += render_pages
+        html += render_pager
 
         html += "\n"
         html += content_tag(:li, :class => "next_page") do
           link_to_page collection.next_page, 'paginate.next_page_label'
         end
         html += "\n"
-        html += content_tag(:li, :class => "last_page") do
-          link_to_page collection.last_page, 'paginate.last_page_label'
+
+        if show_last_page?
+          html += content_tag(:li, :class => "last_page") do
+            link_to_page collection.last_page, 'paginate.last_page_label'
+          end
+          html += "\n"
         end
-        html += "\n"
         html.html_safe
       end
     end
 
-    def render_pages
+    # render pager
+    def render_pager
       html = ""
-
       pager.visible_pages.each do |page|
         html += "\n"
         html += content_tag(:li, :class => "pager") do
@@ -40,6 +53,16 @@ module RailsPaginate::Renderers
       end
       html += "\n"
       html.html_safe
+    end
+
+    # show first page item?
+    def show_first_page?
+      options[:show_first_page] || self.class.show_first_page
+    end
+
+    # show last page item?
+    def show_last_page?
+      options[:show_last_page] || self.class.show_last_page
     end
 
   end
