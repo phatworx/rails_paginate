5791ff3adf810c5fc03fff87f50fad44b268e76d
Marco Scholl <develop@marco-scholl.de>
2011-02-17 18:20:03 +0100

    add more classes for pager

--- lib/rails_paginate/renderers/html_default.rb
+++ lib/rails_paginate/renderers/html_default.rb
@@ -9,6 +9,14 @@ module RailsPaginate::Renderers
     cattr_accessor :show_last_page
     @@show_last_page = true
 
+    # show next page item
+    cattr_accessor :show_next_page
+    @@show_next_page = true
+
+    # show previous page item
+    cattr_accessor :show_previous_page
+    @@show_previous_page = true
+
     def render
       content_tag(:ul) do
         html = "\n"
@@ -20,17 +28,21 @@ module RailsPaginate::Renderers
           html += "\n"
         end
 
-        html += content_tag(:li, :class => "previous_page") do
-          link_to_page collection.previous_page, 'paginate.previous_page_label'
+        if show_previous_page?
+          html += content_tag(:li, :class => "previous_page") do
+            link_to_page collection.previous_page, 'paginate.previous_page_label'
+          end
+          html += "\n"
         end
 
         html += render_pager
 
-        html += "\n"
-        html += content_tag(:li, :class => "next_page") do
-          link_to_page collection.next_page, 'paginate.next_page_label'
+        if show_next_page?
+          html += content_tag(:li, :class => "next_page") do
+            link_to_page collection.next_page, 'paginate.next_page_label'
+          end
+          html += "\n"
         end
-        html += "\n"
 
         if show_last_page?
           html += content_tag(:li, :class => "last_page") do
@@ -44,14 +56,16 @@ module RailsPaginate::Renderers
 
     # render pager
     def render_pager
+      view.reset_cycle(:paginate)
+
       html = ""
-      pager.visible_pages.each do |page|
-        html += "\n"
-        html += content_tag(:li, :class => "pager") do
+      visible_pages = pager.visible_pages
+      visible_pages.each do |page|
+        html += content_tag(:li, :class => "pager #{visible_pages.first == page ? 'first_pager' : ''} #{visible_pages.last == page ? 'last_pager' : ''} #{view.cycle("pager_even", "pager_odd", :name => :paginate)}") do
           link_to_page page, page.nil? ? nil : 'paginate.pager'
         end
+        html += "\n"
       end
-      html += "\n"
       html.html_safe
     end
 
@@ -63,6 +77,16 @@ module RailsPaginate::Renderers
     # show last page item?
     def show_last_page?
       options[:show_last_page] || self.class.show_last_page
+    end
+
+    # show next page item?
+    def show_next_page?
+      options[:show_next_page] || self.class.show_next_page
+    end
+
+    # show previous page item?
+    def show_previous_page?
+      options[:show_previous_page] || self.class.show_previous_page
     end
 
   end
