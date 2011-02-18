49130456c6216ccedb0f33ad1a1052a76bc42ef1
Marco Scholl <develop@marco-scholl.de>
2011-02-19 00:53:14 +0100

    complete rails generator for inital install

--- lib/generators/rails_paginate/install_generator.rb
+++ lib/generators/rails_paginate/install_generator.rb
@@ -2,11 +2,13 @@ module RailsPaginate
   module Generators # :nodoc:
     # Install Generator
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
       desc "Install the rails_paginate plugin"
       def install
-        copy_file "../../../config/initializers/paginate.rb", "config/initializers/paginate.rb"
-        copy_file "../../../config/locales/en.yml", "config/locales/devise.paginate.en.yml"
-        copy_file "../../../config/locales/de.yml", "config/locales/devise.paginate.de.yml"
+        copy_file "config/initializers/paginate.rb", "config/initializers/paginate.rb"
+        copy_file "config/locales/en.yml", "config/locales/paginate.en.yml"
+        copy_file "config/locales/de.yml", "config/locales/paginate.de.yml"
       end
     end
   end
