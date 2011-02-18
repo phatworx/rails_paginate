68153c24556bcd7827bbfbe7dcc5ebe9195c2711
Marco Scholl <develop@marco-scholl.de>
2011-02-18 23:47:16 +0100

    update spec

--- spec/renderers/html_default_spec.rb
+++ spec/renderers/html_default_spec.rb
@@ -24,19 +24,18 @@ describe RailsPaginate::Renderers::HtmlDefault do
         lambda { subject.render }.should_not raise_error(StandardError)
       end
 
-      context "default settings" do
-        context "result of #render" do
-          subject { @renderer.render }
-          specify { should be_an String }
+      context "result of #render" do
+        subject { @renderer.render }
+        specify { should be_an String }
 
-          it("should not have the div at first") { subject.should_not have_tag('> div', :count => 1) }
-          it("should have one ul") { subject.should have_tag('ul') }
-          it("should have one li") { subject.should have_tag('ul > li', :count => 9) }
-          [:first_page, :previous_page, :first_pager, :last_pager, :next_page, :last_page].each do |class_attr|
-            it("should have one li.#{class_attr}") { subject.should have_tag("ul > li.#{class_attr}", :count => 1) }
-          end
-          it("should have one li.pager") { subject.should have_tag('ul > li.pager', :count => 5) }
+        it("should not have the div at first") { subject.should_not have_tag('> div', :count => 1) }
+        it("should have one ul") { subject.should have_tag('ul') }
+        it("should have one li") { subject.should have_tag('ul > li', :count => 9) }
+        [:first_page, :previous_page, :first_pager, :last_pager, :next_page, :last_page].each do |class_attr|
+          it("should have one li.#{class_attr}") { subject.should have_tag("ul > li.#{class_attr}", :count => 1) }
         end
+        it("should have 5 li.pager") { subject.should have_tag('ul > li.pager', :count => 5) }
+        it("should have 8 ul > li > a") { subject.should have_tag('ul > li > a', :count => 8) }
       end
 
 
